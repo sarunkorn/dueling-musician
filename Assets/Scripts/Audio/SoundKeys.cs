e2dca2d12654f4adadbf576e569d9983e2fd7267
SirLight-PC <sarunkorn@sybogames.com>
2023-04-26 20:18:09 +0200

    Import generic script

---
+++ Assets/Scripts/Audio/SoundKeys.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+
+public abstract class SoundKeys : StringEnum<SoundKeys>
+{
+	protected static List<SoundKeys> _soundList = new List<SoundKeys>();
+	protected readonly string _assetKey;
+	protected readonly string _mixerKey;
+	protected SoundKeys(string value, string assetKey, string mixerKey) : base(value)
+	{
+		_assetKey = assetKey;
+		_mixerKey = mixerKey;
+		_soundList.Add(this);
+	}
+
+	public virtual string GetAssetKey()
+	{
+		return _assetKey;
+	}
+
+	public virtual string GetMixerKey()
+	{
+		return _mixerKey;
+	}
+
+	public virtual IEnumerable<SoundKeys> GetAll()
+	{
+		return _soundList;
+	}
+}
---
+++ Assets/Scripts/ResourceManagement/IResourceLoader.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public delegate void SetSpriteHandler(Sprite sprite);
+public interface IResourceLoader
+{
+	Task InitializeAsync();
+	Task Preload(IEnumerable<string> assetKeys);
+	Task<T> Load<T>(string key);
+	Task<T> LoadPrefab<T>(string key);
+	Task<T> LoadJsonAsModel<T>(string key);
+	Task<T> InstantiateAsync<T>(object key, Transform parent = null, bool instantiateInWorldSpace = false, bool trackHandle = true) where T : MonoBehaviour;
+	void Release(System.Object key);
+	void ReleaseInstance(GameObject obj);
+	Task SetSpriteAsync(string key, SetSpriteHandler eventHandler);
+}
